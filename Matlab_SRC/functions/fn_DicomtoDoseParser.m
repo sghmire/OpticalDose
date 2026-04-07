718641f2e7f7ca5d280b3b0ff0379a59e15d7b43
s222330 <sgrghimire07@gmail.com>
2026-04-07 15:55:10 -0500

    complete 4/7/2026

--- Matlab_SRC/functions/fn_DicomtoDoseParser.m
+++
@@ -1,9 +0,0 @@
-function [dicomplane, pixelspacing] = fn_DicomtoDoseParser(dicomfile)
-    info = dicominfo(dicomfile);
-
-    plane = double(squeeze(dicomread(info)));
-    dosescaling = info.DoseGridScaling;
-    dicomplane = plane * dosescaling * 10;   
-
-    pixelspacing = info.PixelSpacing;
-end
--- Matlab_SRC/functions/fn_displayInfo.m
+++
@@ -1,5 +0,0 @@
-function fn_displayInfo(data, update1, update2)
-        pos = ceil(data.CurrentPosition);
-        update1.Value = pos(2);
-        update2.Value = pos(1);
-end
